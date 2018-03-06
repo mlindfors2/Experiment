3aec2fe13cf02c1a8f78a6d61f5090608cc67ca0
mlind <mlindfors2@gmail.com>
2018-03-06 12:18:04 +0100

    20180306 12:18

--- Experiment/src/pg/Mainpg.java
+++ Experiment/src/pg/Mainpg.java
@@ -3,14 +3,17 @@ package pg;
 public class Mainpg {
 	public static void main(String[] args) {
 //		ChatServer cs = new ChatServer(5000,10);
-		ServerController sc = new ServerController(5000,500);
+		ServerController sc = new ServerController(5000,5000);
 		
 		for (int i = 0; i<5000 ; i ++) {
 			String name = "Pelle" + i;
 			new ChatClient("127.0.0.1",5000,name);
 		}
+		for(int i=0;i<sc.getUserList().numberOfUsers();i++) {
 		
-	
+			System.out.println(sc.getUserList().getUser(i).getName());
+		
+		}
 	}
 }
   
